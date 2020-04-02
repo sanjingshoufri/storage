14a296b9e7d2f91732eccb56f3d56c4198e4023c
e <2303386665@qq.com>
2020-04-02 15:45:20 +0800

    cache

---
+++ MySQL/SQL/DDL.sql
@@ -0,0 +1,16 @@
+# 表结构添加一个字段
+ALTER TABLE es_user ADD is_binding_dy tinyint(1) NOT NULL DEFAULT '0' COMMENT '抖音绑定状态';
+
+# 创建表
+CREATE TABLE `es_user_binding_douyin` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(255) DEFAULT '' COMMENT '用户ID',
+  `dy_id` varchar(100) DEFAULT NULL COMMENT '抖音号',
+  `nickname` varchar(255) DEFAULT '' COMMENT '昵称',
+  `profile` varchar(255) DEFAULT '' COMMENT '昵称截图',
+  `status` tinyint(1) DEFAULT '0',
+  `is_delete` tinyint(1) DEFAULT '0' COMMENT '软删除',
+  `create_time` int(11) DEFAULT '0' COMMENT '创建时间',
+  `update_time` int(11) DEFAULT '0' COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='用户基本信息表';
