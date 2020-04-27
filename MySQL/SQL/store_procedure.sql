3065271ec3bdbd1d8c13bcdb8ff18142c8e1db74
TANNER <2303386665@qq.com>
2020-04-27 17:42:25 +0800

    存储过程去重

--- MySQL/SQL/store_procedure.sql
+++ MySQL/SQL/store_procedure.sql
@@ -1,51 +1,41 @@
-/**
- * 定义存储过程
- */
-CREATE PROCEDURE proc7 ()
+# 存储过程删除重复记录
+delimiter $$
+drop procedure if exists `proc_copy_stu_info` $$
+CREATE PROCEDURE `proc_copy_stu_info`()
 BEGIN
+    ##定义变量
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE v_id VARCHAR(200);
+    DECLARE v_last_login_ip VARCHAR(200);
+    DECLARE v_last_login_date VARCHAR(200);
+    DECLARE v_phone_num VARCHAR(200);
 
-DECLARE v INT;
+    # 创建游标，并存储数据
+    DECLARE cur CURSOR FOR SELECT id, last_login_ip, last_login_date, phone_num FROM es_user_login GROUP BY phone_num HAVING COUNT(id) > 1;
 
+    # 游标中的内容执行完后将done设置为true
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = true;
 
-SET v = 0;
+    # 打开游标
+    OPEN cur;
+       # 执行循环
+       read_loop : LOOP
 
-sanorder :
-LOOP
-	INSERT INTO sanorder (
-		user_id,
-		goods_id,
-		goods_num,
-		description
-	) SELECT
-		user_id,
-		goods_id,
-		goods_num,
-		description
-	FROM
-		sanorder;
+       IF done THEN
+           LEAVE read_loop;
+       END IF;
 
+       # 取游标中的值
+       FETCH cur INTO v_id, v_last_login_ip, v_last_login_date, v_phone_num;
 
-SET v = v + 1;
+       IF v_last_login_ip IS NULL AND v_last_login_date = 0 THEN
+           INSERT INTO es_del_user_login(user_id, phone_num) VALUE(v_id, v_phone_num);
+       END IF;
 
-
-IF v >= 5 THEN
-	LEAVE sanorder;
-
-
+       END LOOP read_loop;
+    CLOSE cur;
 END
-IF;
-
-
-END
-LOOP
-;
-
-
-END;
-
-
-
-/**
- * 调用存储过程
- */
-call proc7 () 
+$$
+delimiter;
+call `proc_copy_stu_info`();
+commit;
