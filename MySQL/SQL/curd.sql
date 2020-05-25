e9df9f9fc6e88e8a8712fed05e07c0f35293f2af
TANNER <2303386665@qq.com>
2020-05-25 18:23:11 +0800

    mysql

--- MySQL/SQL/curd.sql
+++ MySQL/SQL/curd.sql
@@ -3,3 +3,11 @@
  */
 DELETE FROM table WHERE id = (SELECT id FROM (SELECT id FROM table ORDER BY id desc LIMIT 1) a)
 
+/**
+ * 删除重复记录，保留最后一条记录
+ */
+DELETE c1 FROM table c1
+INNER JOIN table c2 
+WHERE
+    c1.id > c2.id AND 
+    c1.phone_num = c2.phone_num;
