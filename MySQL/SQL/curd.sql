6515fc09ce6468f8946702b439aedd476e59d147
TANNER <2303386665@qq.com>
2020-09-11 18:17:38 +0800

    sql list

--- MySQL/SQL/curd.sql
+++ MySQL/SQL/curd.sql
@@ -12,3 +12,18 @@ WHERE
 
 # 清空表
 TRUNCATE table;
+
+
+# 查询插入
+INSERT INTO es_user_drop (
+	`drop`,
+	`user_id`,
+	`create_time`,
+	`update_time`
+) SELECT
+	num,
+	user_id,
+	unix_timestamp(now()),
+	unix_timestamp(now())
+FROM
+	table
